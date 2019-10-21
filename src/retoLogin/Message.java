9b917a451f726c9e41eef5043440281abf01c984
2dam <2dam@HZ341338.TartangaLH.eus>
2019-10-21 11:33:57 +0200

    Añadido codigo a las clases

--- src/retoLogin/Message.java
+++ src/retoLogin/Message.java
@@ -6,9 +6,38 @@
 package retoLogin;
 
 /**
- *
- * @author 2dam
+ * The class that will be used as the message by the sockets.
+ * @author Jon
  */
 public class Message {
+    //Attributes
+    private int type;
+    private User user;
+    
+    //Constructors
+    public Message() {
+    }
+
+    public Message(int type, User user) {
+        this.type = type;
+        this.user = user;
+    }
+    
+    //Getters and Setters
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
     
 }
--- src/retoLogin/User.java
+++ src/retoLogin/User.java
@@ -5,10 +5,112 @@
  */
 package retoLogin;
 
+import java.sql.Timestamp;
+
 /**
- *
- * @author 2dam
+ * The java bean of the user, to be used in the Message.
+ * @author Jon
  */
 public class User {
+    //Attributes
+    private int id;
+    private String login;
+    private String email;
+    private String fullName;
+    private int status;
+    private int privilege;
+    private String password;
+    private Timestamp lastAccess;
+    private Timestamp lastPasswordChange;
+
+    //Constructors
+    public User() {
+    }
+
+    public User(int id, String login, String email, String fullName, int status, int privilege, String password, Timestamp lastAccess, Timestamp lastPasswordChange) {
+        this.id = id;
+        this.login = login;
+        this.email = email;
+        this.fullName = fullName;
+        this.status = status;
+        this.privilege = privilege;
+        this.password = password;
+        this.lastAccess = lastAccess;
+        this.lastPasswordChange = lastPasswordChange;
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(int privilege) {
+        this.privilege = privilege;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getLastAccess() {
+        return lastAccess;
+    }
+
+    public void setLastAccess(Timestamp lastAccess) {
+        this.lastAccess = lastAccess;
+    }
+
+    public Timestamp getLastPasswordChange() {
+        return lastPasswordChange;
+    }
+
+    public void setLastPasswordChange(Timestamp lastPasswordChange) {
+        this.lastPasswordChange = lastPasswordChange;
+    }
+    
     
 }
